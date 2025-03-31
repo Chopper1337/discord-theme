81c4554c697d99bcd81373ceadc70b94114cf22f
Chopper <43015696+Chopper1337@users.noreply.github.com>
2025-03-31 16:19:15 +0100

    Update

--- update.sh
+++ update.sh
@@ -22,11 +22,11 @@ touch theme.css || exit
 (cd HorizontalServerList && npm i && npm run build)
 
 # Create theme.css
-cat HorizontalServerList/dist/dist/HorizontalServerList.css > theme.css || echo "Failed to add HorizontalServerList.css to theme.css"
+cat ./HorizontalServerList/dist/HorizontalServerList.css > theme.css || echo "Failed to add HorizontalServerList.css to theme.css"
 printf "\n" >> theme.css
-cat midnight-discord/build/midnight.css >> theme.css || echo "Failed to add midnight-discord/midnight.css to theme.css"
+cat ./midnight-discord/build/midnight.css >> theme.css || echo "Failed to add midnight-discord/midnight.css to theme.css"
 printf "\n" >> theme.css
-cat amoled-cord/src/amoled-cord.css >> theme.css || echo "Failed to add amoled-cord.css to theme.css"
+cat ./amoled-cord/src/amoled-cord.css >> theme.css || echo "Failed to add amoled-cord.css to theme.css"
 
 minify theme.css > theme.min.css || echo "Failed to minify theme.css"
 
